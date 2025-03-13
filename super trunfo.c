903197338bfbb159760c959b59f04474e582260e
fabricio <fabriciodamorim@gmail.com>
2025-03-13 20:41:08 +0000

    finalizado

--- super trunfo.c
+++ super trunfo.c
@@ -9,32 +9,67 @@ int main(){
   int pontos;
   float area;
   float pib;
-  //printf para o usuario saber a informação que queremos e scanf para ler os dados do usuario
-  printf("Digite a carta: \n");
+  printf("Digite O Número Da Carta: \n");
   scanf("%s", &carta);
 
-  printf("Digite o estado: \n");
+  printf("Digite O Nome Estado: \n");
   scanf("%s", &estado);
 
-  printf("Digite o codigo: \n");
+  printf("Digite O Número Do Código: \n");
   scanf("%s", &codigo);
 
-  printf("Digite a cidade: \n");
+  printf("Digite O Nome Da Cidade: \n");
   scanf("%s", &cidade);
 
-printf("Digite a população: \n");
+printf("Digite O Número De Habitantes: \n");
 scanf("%d", &populacao);
 
-printf("Digite a Àrea: \n");
+printf("Digite O Tamanho Da Àrea: \n");
 scanf("%f", &area);
 
-printf("Digite o Pib da cidade: \n");
+printf("Digite O Pib Da Cidade: \n");
 scanf("%f", &pib);
 
-printf("Digite o número de pontos turisticos: \n");
+printf("Digite O Número De Pontos Turisticos: \n");
 scanf("%d",&pontos);
 
-//coloquei todas as informações para serem exibidas uma abaixo da outra
+
+  printf("Carta: %s \n",  carta);
+  printf("Estado: %s \n", estado);
+  printf("Codigo: %s \n", codigo);
+  printf("Nome da Cidade: %s \n", cidade);
+  printf("População: %d \n", populacao);
+  printf("Àrea: %f km² \n",area);
+  printf("PIB: %.3f Bilhões de Reais \n",pib);
+  printf("Número de Pontos Turisticos: %d \n", pontos);
+
+
+
+  printf("Digite O Número Da Carta: \n");
+  scanf("%s", &carta);
+
+  printf("Digite O Nome Estado: \n");
+  scanf("%s", &estado);
+
+  printf("Digite O Número Do Código: \n");
+  scanf("%s", &codigo);
+
+  printf("Digite O Nome Da Cidade: \n");
+  scanf("%s", &cidade);
+
+printf("Digite O Número De Habitantes: \n");
+scanf("%d", &populacao);
+
+printf("Digite O Tamanho Da Àrea: \n");
+scanf("%f", &area);
+
+printf("Digite O Pib Da Cidade: \n");
+scanf("%f", &pib);
+
+printf("Digite O Número De Pontos Turisticos: \n");
+scanf("%d",&pontos);
+
+
   printf("Carta: %s \n",  carta);
   printf("Estado: %s \n", estado);
   printf("Codigo: %s \n", codigo);
@@ -44,9 +79,11 @@ scanf("%d",&pontos);
   printf("PIB: %.3f Bilhões de Reais \n",pib);
   printf("Número de Pontos Turisticos: %d \n", pontos);
   
-  
-  };
-  
+
+}
+
+
+
 
 
 
